3385ca06ee38af96a6b20df491b6828c995ae42d
Jonathan <ea1het@ea1het.com>
2019-07-02 02:14:35 +0200

    Included an execution time counter

--- app.py
+++ app.py
@@ -7,12 +7,22 @@ __author__ = 'ea1het'
 
 from geopy import distance
 import json
+import time
 
 MADRID = (40.4168, -3.7038)
+NUM = 10
+
+start = time.time()
 
 with open('cities.json') as ciudades:
     data = json.load(ciudades)
 
-    for p in data:
-        print('\nCiudad: ' + p['name'])
-        print('Distancia a MADRID: %.2f km' % distance.distance(MADRID, (p['lat'], p['lng'])).km)
+i = 0
+for p in data:
+    print('\nCiudad: ' + p['name'])
+    print('Distancia a MADRID: %.2f km' % distance.distance(MADRID, (p['lat'], p['lng'])).km)
+    i += 1
+
+end = time.time()
+
+print('\nEvaluadas un total de %s ciudades en %s segundos' % (i, round((end-start),2)))
